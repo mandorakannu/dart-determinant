264926c5ada4d826c4688f0aaca4c9bd7b5d6489
mandorakannu <mandorakannu@gmail.com>
2023-09-13 10:35:35 +0530

    GIT INIT

---
+++ bin/determinant.dart
@@ -0,0 +1,5 @@
+import 'package:determinant/determinant.dart' as determinant;
+
+void main(List<String> arguments) {
+  print('Hello world: ${determinant.calculate()}!');
+}
---
+++ test/determinant_test.dart
@@ -0,0 +1,8 @@
+import 'package:determinant/determinant.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
